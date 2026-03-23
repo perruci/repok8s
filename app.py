4195e6c6c01a9810aac842ccda2b143af210966c
Bruno Perruci <perruci.bruno@gmail.com>
2026-03-23 12:18:43 -0300

    Ajustando permissao para PR - agora vai

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Testando o promote via PR para ambiente de HML"
+    return "Testando o promote via PR para ambiente de HML - AGORA VAI"
 
 app.run(host="0.0.0.0", port=5000)
